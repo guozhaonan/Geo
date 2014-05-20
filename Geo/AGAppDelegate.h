2c5a5bb5ea8ba5e71ab10a00e2f7a6451299fe7a
Albert Guo <guozhaonan@gmail.com>
2014-05-20 12:32:33 -0400

    Initial Commit

---
+++ Geo/AGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AGAppDelegate.h
+//  Geo
+//
+//  Created by Albert Guo on 5/20/14.
+//  Copyright (c) 2014 Albert Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Geo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Geo
+//
+//  Created by Albert Guo on 5/20/14.
+//  Copyright (c) 2014 Albert Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AGAppDelegate class]));
+    }
+}
